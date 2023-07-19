000f208c31516053220c1a51192468f9a3dd97d6
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-07-19 22:48:43 +0200

    - Dividir en componentes la máqueta en Angular - Revisión CSS Vue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NoEncontradoComponent } from './components/no-encontrado/no-encontrado.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'registro', component: RegistroComponent},
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path:'**', component: NoEncontradoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { NoEncontradoComponent } from './components/no-encontrado/no-encontrado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    HomeComponent,
+    RegistroComponent,
+    NoEncontradoComponent
   ],
   imports: [
     BrowserModule,
