a69149a830c146123aac10b0c41da45d61996f27
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2025-05-16 21:13:57 +0200

    añadir script test-headless y test para SweetAlert2

--- src/app/components/acceso/acceso.component.spec.ts
+++ src/app/components/acceso/acceso.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 import { AccesoComponent } from './acceso.component';
 
@@ -8,14 +11,24 @@ describe('AccesoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AccesoComponent]
+      declarations: [AccesoComponent],
+      providers: [
+        { provide: AuthService, useValue: { login: () => ({ subscribe: (callbacks: any) => callbacks.next({ token: 'fake-token' }) }), register: () => {} } },
+        { provide: Router, useValue: { navigate: jasmine.createSpy('navigate') } }
+      ]
     });
     fixture = TestBed.createComponent(AccesoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería llamar a Swal.fire al loguear correctamente', () => {
+    const swalSpy = spyOn(Swal, 'fire');
+    component.loginUsuario();
+    expect(swalSpy).toHaveBeenCalledWith('Acceso','Logeado correctamente');
   });
 });
