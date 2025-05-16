2d2d67b98550712220de53efaf4ae401696e2e43
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2025-05-16 18:39:26 +0200

    revisión de Cursor la actualización con 'npm audit' y añadir test para componentes que hacen uso de SweetAlert2

--- src/app/components/carrito/carrito.component.spec.ts
+++ src/app/components/carrito/carrito.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CarritoService } from '../../services/carrito.service';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 import { CarritoComponent } from './carrito.component';
 
@@ -8,14 +12,26 @@ describe('CarritoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CarritoComponent]
+      declarations: [CarritoComponent],
+      providers: [
+        { provide: CarritoService, useValue: { clean: jasmine.createSpy('clean'), myCart$: { subscribe: () => {} }, isEmpty: () => true, totalCart: () => 0, deleteCurso: () => {} } },
+        { provide: AuthService, useValue: { isLoginUser: () => false } },
+        { provide: ActivatedRoute, useValue: {} },
+        { provide: Router, useValue: { navigate: jasmine.createSpy('navigate') } }
+      ]
     });
     fixture = TestBed.createComponent(CarritoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería llamar a Swal.fire cuando el usuario no está logueado al comprar', () => {
+    const swalSpy = spyOn(Swal, 'fire');
+    component.comprar();
+    expect(swalSpy).toHaveBeenCalledWith('Compra','Debe estar logeado para realizar la compra');
   });
 });
