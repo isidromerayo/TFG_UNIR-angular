c671ab1cc0f035cc397d1f846daed8a597217687
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2026-01-26 17:08:05 +0100

    Revert "fix: add missing categories API call mock"
    This reverts commit a81994dc5449d0aae59aef429ce10ab128f50780.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -28,19 +28,10 @@ describe('My First Test', () => {
       }
     }).as('getCursosUltimas');
 
-    cy.intercept('GET', 'http://localhost:8080/api/categorias?sort=nombre&size=5', {
-      statusCode: 200,
-      body: {
-        _embedded: {
-          categorias: []
-        }
-      }
-    }).as('getCategorias');
-
     cy.visit('/');
     
     // Wait for API calls to complete
-    cy.wait(['@getCursosDestacados', '@getOpiniones', '@getCursosUltimas', '@getCategorias']);
+    cy.wait(['@getCursosDestacados', '@getOpiniones', '@getCursosUltimas']);
     
     // Verify the page loads correctly
     cy.get('h2').contains('Encuentra tu curso');
