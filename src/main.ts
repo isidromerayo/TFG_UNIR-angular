ef1fefe3b420b5414dd3206740b6c80d39c057d4
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2026-09-13 18:43:50 +0200

    test: e2e de regresion para render de datos asincronos (NoopNgZone) + refactor tipado del bootstrap en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection, type EnvironmentProviders, type Provider } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app-routing.module';
 
+const appProviders: (Provider | EnvironmentProviders)[] = [
+  provideRouter(routes),
+  provideHttpClient(withInterceptorsFromDi()),
+  provideZoneChangeDetection({ eventCoalescing: true })
+];
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
-    provideZoneChangeDetection({ eventCoalescing: true })
-  ]
-})
+bootstrapApplication(AppComponent, { providers: appProviders })
   .catch(err => console.error(err));
