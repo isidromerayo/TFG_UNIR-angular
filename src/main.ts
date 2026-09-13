10e81d02d0ba1d5d6bb084274942e71104d3c00f
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2026-09-13 18:54:55 +0200

    test: mover regresion NoopNgZone a spec.cy.ts y simplificar main.ts (responde hallazgos Codacy)

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideZoneChangeDetection, type EnvironmentProviders, type Provider } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app-routing.module';
 
-const appProviders: (Provider | EnvironmentProviders)[] = [
-  provideRouter(routes),
-  provideHttpClient(withInterceptorsFromDi()),
-  provideZoneChangeDetection({ eventCoalescing: true })
-];
-
-bootstrapApplication(AppComponent, { providers: appProviders })
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideZoneChangeDetection({ eventCoalescing: true })
+  ]
+})
   .catch(err => console.error(err));
